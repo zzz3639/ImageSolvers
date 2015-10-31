f552b32cb248cd61759a94f416133c17eed1eb0d
Zhang Haowen <pomoncn384@gmail.com>
2015-10-31 17:49:50 +0800

    non-even noise modified

--- Empirical/RunSolverFix.m
+++ Empirical/RunSolverFix.m
@@ -1,7 +1,8 @@
 function [ pic,no,mv ] = RunSolverFix( bm, OptPara, StopPara, mv0 )
 %   Modified in 2015.08.21, by ZHANG Haowen
-%   Usage: [pic,no,mv,Time] = RunSolverFix(bm, Lambda, [sigma, bsize, bdecay], [MaxIte,Izero,Pzero], save/kframes, mv0);  
-
+%   Usage: [pic,no,mv,Time] = RunSolverFix(bm, Lambda, [sigma,bsize,psfdecay] or [sigma,bsize,bdecay,NoiseGridSize1,NoiseGridSize2], [MaxIte,Izero,Pzero], save/kframes, mv0);  
+%   if OptPara=[sigma,bsize,psfdecay], then the background noise is considered to be even
+%   Otherwise noise is modeled by first order b-spline base.
 
 %%% define image size and optimization parameters
 n=size(mv0.pic,1);
